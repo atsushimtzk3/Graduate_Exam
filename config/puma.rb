b727f4a71b80800128d9b72b38f29bf309c6f165
Atsushi <atsushimtzk3@gmail.com>
2018-08-28 02:26:53 +0000

    test commit

--- config/puma.rb
+++ config/puma.rb
@@ -52,5 +52,11 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 #
 
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
